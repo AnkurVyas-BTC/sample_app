a575c0ceb52f878050078d815b5197e81dbed6e4
AnkurVyas-BTC <ankur.vyas@beaverlogic.com>
2014-07-03 14:11:53 +0530

    Add user microposts

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -54,8 +54,7 @@ describe "Authentication" do
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
 
-
-      describe "when attempting to visit a protected page" do
+        describe "when attempting to visit a protected page" do
 
           before do
             visit edit_user_path(user)
@@ -64,12 +63,26 @@ describe "Authentication" do
             click_button "Sign in"
           end
 
-        describe "after sigining in " do
+          describe "after sigining in " do
           it "should render the desired protected page" do
             expect(page).to have_title('Edit user')
           end
+          end
         end
-      end
+
+
+        describe "in the Microposts controller" do
+            describe "submitting to the create action" do
+              before { post microposts_path}
+              specify { expect(response).to redirect_to(signin_path) }
+            end
+
+            describe "submitting to the destroy action" do
+              before{ delete micropost_path(FactoryGirl.create(:micropost)) }
+              specify { expect(response).to redirect_to(signin_path) }
+            end
+        end
+
 
 
       describe "in the Users controller" do
